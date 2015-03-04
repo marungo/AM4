9c6cf4242f9cdec7d63b97445152cf4070a3c705
Mary Ruth Ngo <mngo@wellesley.edu>
2015-03-03 22:22:24 -0500

    course feed

--- am4.js
+++ am4.js
@@ -1,1 +1,30 @@
-am4.js
+var url = "https://spreadsheets.google.com/feeds/list/1035SQBywbuvWHoVof3G-VI0rapYJslaeYN5tTpNZq2M/od6/public/values?";
+var cleanedCourses = {}// global variable
+
+$.getJSON(url+"alt=json-in-script&callback=?",
+    function (response){
+      console.log(response);
+      items = response.feed.entry;
+      if (response.feed) {
+        console.log(response.feed.entry.length);
+        processCourses(items);
+      }
+
+});
+
+function processCourses(allCourses){   
+  // 1. create an object that will store each course by its CRN
+  for (var i in allCourses){
+    var course = allCourses[i];
+    var crsObj = {'CRN': course["gsx$crn"]["$t"],
+                  'Name': course["gsx$course"]["$t"],
+                  'Title': course["gsx$title"]["$t"],
+                  // more fields here
+                  'Days': course["gsx$days"]["$t"].split("\n")[1], // clean up days
+                  //continue with fields
+                  };
+    //2. Add it to the cleanedCourses object, using the CRN value as the property name.
+    cleanedCourses[crsObj.CRN] = crsObj;
+  }
+  // other things
+}   am4.js
