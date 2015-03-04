0fcac8d2910b5b390fea075eba4b5be00e934aa2
Mary Ruth Ngo <mngo@wellesley.edu>
2015-03-04 10:14:24 -0500

    populating course feed js file

--- am4.js
+++ am4.js
@@ -1,15 +1,16 @@
 var url = "https://spreadsheets.google.com/feeds/list/1035SQBywbuvWHoVof3G-VI0rapYJslaeYN5tTpNZq2M/od6/public/values?";
 var cleanedCourses = {}// global variable
+var allSubjects = [];
+var courseArray = [];
 
 $.getJSON(url+"alt=json-in-script&callback=?",
     function (response){
-      console.log(response);
       items = response.feed.entry;
       if (response.feed) {
-        console.log(response.feed.entry.length);
         processCourses(items);
+        parseCoursesInfo(cleanedCourses);
+        console.log(courseArray);
       }
-
 });
 
 function processCourses(allCourses){   
@@ -19,12 +20,58 @@ function processCourses(allCourses){
     var crsObj = {'CRN': course["gsx$crn"]["$t"],
                   'Name': course["gsx$course"]["$t"],
                   'Title': course["gsx$title"]["$t"],
-                  // more fields here
                   'Days': course["gsx$days"]["$t"].split("\n")[1], // clean up days
-                  //continue with fields
+                  'Times': course["gsx$meetingtimes"]["$t"],
+                  'Distributions': course["gsx$distributions"]["$t"],
+                  'Instructor': course["gsx$instructor"]["$t"]
                   };
     //2. Add it to the cleanedCourses object, using the CRN value as the property name.
     cleanedCourses[crsObj.CRN] = crsObj;
   }
   // other things
-}   am4.js
+}
+
+function parseCoursesInfo(courses) {
+	for (var key in courses) {
+		courseArray.push(courses[key].Name.split(" "));//index 0 = subject, 1 = course level/num, 3 = section
+		courseArray[courseArray.length-1][2] = (courses[key].CRN);//index 2
+		courseArray[courseArray.length-1].push(courses[key].Title);//index 4
+		courseArray[courseArray.length-1].push(courses[key].Days);//index 5
+		courseArray[courseArray.length-1].push(courses[key].Times);//index 6
+		courseArray[courseArray.length-1].push(courses[key].Distributions);//index 7
+		courseArray[courseArray.length-1].push(courses[key].Instructor);//index 8
+	}
+}
+
+//give the index of the constraint (based on its 
+//placement in a courseArray element)
+function findCoursesBOConstraint(constraint, index) {
+	var courses = courseArray;
+	var results = [];
+	for (var i in courses) {
+		if (courses[i][index] == constraint) {
+			results.push(courses[i]);
+		}
+	}
+	return results;
+}
+
+
+var button = document.querySelector("button");
+button.onclick = function () {
+    var input = document.querySelector("#guess");
+    input = input.value;
+    //get info
+}
+
+console.log(cleanedCourses);
+
+console.log(findCoursesBOConstraint("AFR", 0));
+
+
+
+
+
+
+
+
