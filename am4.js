327c1fcb78bc5ce07aae2eb3318e2b543b2c40d9
Mary Ruth Ngo <mngo@wellesley.edu>
2015-03-08 16:50:49 -0400

    Converge Lucy's code with MRs

--- am4.js
+++ am4.js
@@ -1,18 +1,84 @@
+//global variables!
 var url = "https://spreadsheets.google.com/feeds/list/1035SQBywbuvWHoVof3G-VI0rapYJslaeYN5tTpNZq2M/od6/public/values?";
-var constraints = [["AFR", 0], ["Pashington Obeng", 8]];//temporary global variable to test constraint functions
+var cleanedCourses = {};
+var courseArray = [];
 
-$.getJSON(url+"alt=json-in-script&callback=?",
-    function (response){
-      items = response.feed.entry;
-      if (response.feed) {
-        processCourses(items);
-        parseCoursesInfo(cleanedCourses);
-        console.log(courseArray);
-        console.log(findCoursesBOAllConstraints(constraints));
-      }
+$(document).ready(function () {
+  $('.about').click(function() {
+    $('.info').css('visibility', 'visible');
+    $('.about').css('visibility','hidden');
+  });
+
+  $('.info').click(function() {
+    $('.info').css('visibility', 'hidden');
+    $('.about').css('visibility','visible');
+  });
+
+  $(function() {
+    var subjects = [
+      "Africana Studies", "American Studies","Anthropology","Arabic",
+      "Art History","Art-Studio","Astronomy","Biochemistry","Biological Science",
+      "Cinema and Media Studies","Chemistry","Chinese Language and Culture",
+      "Classical Civilization","Cognitive and Linguistic Sci",
+      "Comparative Literature","Computer Science","East Asian Languages and Cultures",
+      "Economics","Education","English","Engineering","Environmental Studies",
+      "Extradepartmental","French","Geosciences","German","Greek","Hebrew",
+      "History","Hindi/Urdu","Italian Studies","Japanese Lang and Culture",
+      "Korean Lang and Culture","Latin","Linguistics","Mathematics",
+      "Medieval/Renaissance","Middle Eastern Studies","Music","Neuroscience",
+      "Physical Education","Peace and Justice Studies","Philosophy","Physics",
+      "Political Science (POL)","Political Science (POL1)","Political Science (POL2)",
+      "Political Science (POL3)","Political Science (POL4)","Portuguese",
+      "Psychology","Quantitative Reasoning","Russian Area Studies","Religion",
+      "Russian","South Asia Studies","Sociology","Spanish","Sustainability",
+      "Swahili","Theatre Studies","Women's & Gender Studies","Writing", ];
+    var abbr = [
+      "AFR", "AMST", "ANTH","ARAB","ARTH","ARTS", "ASTR", "BABS", "BIOC", "BISC",
+      "CAMS","CHEM","CHIN","CLCV","CLSC","CPLT","CS","EALC","ECON","EDUC","ENG",
+      "ENGR","ES","EXTD","FREN","GEOS","GER","GRK","HEBR","HIST","HNUR",
+      "ITAS","JPN","KOR","LAT","LING","MATH","ME/R","MES","MUS","NEUR",
+      "PE","PEAC","PHIL","PHYS","POL","POL1","POL2","POL3","POL4",
+      "PORT","PSYC","QR","RAST","REL","RUSS","SAS","SOC","SPAN","SUST",
+      "SWA","THST", "WGST","WRIT",];
+    
+    $('#classes').autocomplete({
+      source: subjects
+    });
+  });
+
+  $('#check').buttonset();
+
+  $("#submitInfo").click(function() {
+        var subject = $("#classes").val();
+        var distribution = $('#menu').val();
+        var days = [];
+        $('#check input:checked').each(function() {
+          days.push($(this).attr('id'));
+        });   
+        console.log(subject);
+        console.log(distribution);
+        console.log(days);
+  });
 });
 
-var cleanedCourses = {}// global variable
+
+$.getJSON(url+"alt=json-in-script&callback=?", callbackFunction(response)});
+
+function callbackFunction(response) {
+  items = response.feed.entry;
+  if (response.feed) {
+    processCourses(items);
+    parseCoursesInfo(cleanedCourses);
+
+    for (var i in cleanedCourses) {
+
+    }
+
+    console.log(courseArray);
+    console.log(findCoursesBOConstraint("AFR", 0));
+  }
+}
+
 function processCourses(allCourses){   
   // 1. create an object that will store each course by its CRN
   for (var i in allCourses){
@@ -31,7 +97,6 @@ function processCourses(allCourses){
   // other things
 }
 
-//make a course object as an array of course attribute elements
 function parseCoursesInfo(courses) {
 	for (var key in courses) {
 		courseArray.push(courses[key].Name.split(" "));//index 0 = subject, 1 = course level/num, 3 = section
@@ -44,11 +109,10 @@ function parseCoursesInfo(courses) {
 	}
 }
 
-//give a set of courses, the constraint and the index 
-//of the constraint (based on its placement in a courseArray 
-//element), return an array of courses that meet that constraint
-var courseArray = []//global variable
-function findCoursesBOConstraint(courses, constraint, index) {
+//give the index of the constraint (based on its 
+//placement in a courseArray element)
+function findCoursesBOConstraint(constraint, index) {
+	var courses = courseArray;
 	var results = [];
 	for (var i in courses) {
 		if (courses[i][index] == constraint) {
@@ -58,24 +122,12 @@ function findCoursesBOConstraint(courses, constraint, index) {
 	return results;
 }
 
-//constraints is an array of all the constraints
-//each constraint element is a two-element array with
-//the query (taken from drop-down menu) and corresponding index
-function findCoursesBOAllConstraints(constraints) {
-	var courses = courseArray;
-	for (var i in constraints) {
-		courses = findCoursesBOConstraint(courses, 
-			constraints[i][0], constraints[i][1]);
-	}
-	return courses;
+function searchSubjectQuery(query) {
+  var index;
+  for (var i in subjects) {
+    if (query == subjects[i]) {
+      index = i;
+    }
+  }
+  return abbr[index];
 }
-
-var button = document.querySelector("button");
-button.onclick = function () {
-    var input = document.querySelector("#guess");
-    input = input.value;
-    //get info
-}
-
-
-
