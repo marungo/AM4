e42a07f49aaa1c2e9d64ff33290ac2221cc1b310
Mary Ruth Ngo <mngo@wellesley.edu>
2015-03-11 15:34:03 -0400

    Pushing just in case! Woot!

--- am4.js
+++ am4.js
@@ -8,7 +8,7 @@ meet the given criteria will be displayed in a div, and ultimately be used to po
 a Google Calendar (that functionality is in the other js file).
 
 Honor Code Statement:
-On this task I collaborated with Lucy Cheng and Jessica Bu. Lucy focused on the CSS and HTML
+On this task I collaborated with Lucy Cheng and Jessica Bu. Lucy focused on the CSS, HTML and jQuery
 while Jessica and I focused on back-end Javascript stuff. I worked with the course feed and Jessica
 worked on exporting a class schedule onto the Google Calendar. We had a lot of fun!
 
@@ -18,6 +18,7 @@ worked on exporting a class schedule onto the Google Calendar. We had a lot of f
 var url = "https://spreadsheets.google.com/feeds/list/1035SQBywbuvWHoVof3G-VI0rapYJslaeYN5tTpNZq2M/od6/public/values?";
 var cleanedCourses = [];
 var courseArray = [];
+var selectedCourses = [];
 var subjects, abbr;
 
 $(document).ready(function () {
@@ -81,8 +82,27 @@ $(document).ready(function () {
       days.push($(this).attr('id'));
     });
 
-    var courses = findCoursesALLConstraints(courseArray, subject, distribution, days);
+    var courses = findCoursesALLConstraints(subject, distribution, days);
   });
+});
+
+$(document).on("click", 'button', function() {
+  console.log(this);
+  var index = selectedCourses.indexOf(findCoursesByCRN(this.id));
+  if (index == -1) {
+    selectedCourses.push(findCoursesByCRN(this.id));
+    console.log(selectedCourses);
+    var added = $(this).clone().appendTo('#calendar');
+    $(added).css("width", "170px");
+    $(added).css("border", "1px auto black");
+  }
+});
+
+$(document).on("click", '#calendar button', function() {
+  $(this).remove();
+  var index = selectedCourses.indexOf(findCoursesByCRN(this.id));
+  selectedCourses.splice(index, 1);
+  console.log(selectedCourses + "after removal");
 });
 
 //important function that makes an .ajax request that pulls data from a Google Spreadsheet
@@ -117,98 +137,102 @@ function processCourses(allCourses){
 //go through the cleanedCourse array and make an array of courses that I could visualize
 //a little better than the objects built in processCourses function.
 function parseCoursesInfo(courses) {
-	for (var key in courses) {
-		courseArray.push(courses[key].Name.split(" "));//index 0 = subject, 1 = course level/num, 3 = section
-		courseArray[courseArray.length-1][2] = (courses[key].CRN);//index 2
-		courseArray[courseArray.length-1].push(courses[key].Title);//index 4
+  for (var key in courses) {
+    courseArray.push(courses[key].Name.split(" "));//index 0 = subject, 1 = course level/num, 3 = section
+    courseArray[courseArray.length-1][2] = (courses[key].CRN);//index 2
+    courseArray[courseArray.length-1].push(courses[key].Title);//index 4
     courses[key].Days = courses[key].Days.replace(/\d/g, "").replace(/Th/g, "R").replace(/(.)(?=.*\1)/g, "");
     courses[key].Days = courses[key].Days.split("\n");
     courses[key].Days = courses[key].Days.slice(1, courses[key].Days.length);
-		courseArray[courseArray.length-1].push(courses[key].Days);//index 5
-		courseArray[courseArray.length-1].push(courses[key].Times);//index 6
-		courseArray[courseArray.length-1].push(courses[key].Distributions);//index 7
-		courseArray[courseArray.length-1].push(courses[key].Instructor);//index 8
-	}
+    courseArray[courseArray.length-1].push(courses[key].Days);//index 5
+    courseArray[courseArray.length-1].push(courses[key].Times);//index 6
+    courseArray[courseArray.length-1].push(courses[key].Distributions);//index 7
+    courseArray[courseArray.length-1].push(courses[key].Instructor);//index 8
+  }
 }
 
-//returns the courses of the given subject. The easiest search function! No cases!
-function findCoursesSub(courses, subject) {
-	var results = [];
-	for (var i in courses) {
-		if (courses[i][0] == subject) {
-			results.push(courses[i]);
-		}
-	}
-	return results;
+function checkSubject(course, subject) {
+  console.log(course[0] + " : " + subject);
+  if (course[0] == subject) {
+    console.log("YAY! ^");
+    return true;
+  }
+  return false;
 }
 
-//returns courses that fit the exact combination of days that user selects
-function findCoursesDays(courses, daysArray) {
-  var results = [];
-  for (var i in courses) {
-    
-    //to nullify the partitioned days, join everything, take away the commas, and resplit
-    var days = courses[i][5].join().replace(/,/g, "");
-    days = days.split("");
+function checkDays(course, daysArray) {    
+  //to nullify the partitioned days, join everything, take away the commas, and resplit
+  var days = course[5].join().replace(/,/g, "");
+  days = days.split("");
 
-    // console.log(JSON.stringify(days) + " : " + JSON.stringify(daysArray));
-    
-    //stringify the arrays so that they can be compared to. If you just compare
-    //arrays, they are not pointing to the same object, so even if they look the same,
-    //they will return as false.
-    if (JSON.stringify(days) == JSON.stringify(daysArray)) {//if array of days is equal to the days selected
-      results.push(courses[i]);
+  for (var i in daysArray) {
+    for (var j in days) {
+      if (daysArray[i] ==  days[j]) {//if 1 of the selected days is in the days of a course
+        return true;
+      }
     }
   }
-  return results;
+
+  return false;
 }
 
 //returns courses that fulfill the distribution that the user selects
-function findCoursesDistrib(courses, distribution) {
+function checkDistribs(course, distribution) {
+  if (course[7].length > 1) {//if multiple distributions
+    for (j = 0; j<course[7].length;) {
+      if (course[7][j] == distribution) {  
+        return true;      
+      }
+      j += 2;//increment by 2 because every odd number array
+      //element is fluff, no content
+    }
+  } else if (course[7].length == 1) {//if only one distribution, or distribution is separated by "or"
+    var distribs = course[7];
+    distribs = distribs[0].split(" or ");
+    for (var j in distribs) {//in case there are 2 distributions split by "or"
+      if (distribs[j] == distribution) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//this function iterates through the 774 courses exactly once and checks
+//each course against the given criteria. Function works even if not all of
+//the constraints are activated.
+function checkAllConstraints(subject, distribution, days) {
   var results = [];
-  for (var i in courses) {    if (courses[i][7].length > 1) {//if multiple distributions
-      for (j = 0; j<courses[i][7].length;) {
-        if (courses[i][7][j] == distribution) {        
-          results.push(courses[i]);
-        }
-        j += 2;//increment by 2 because every odd number array
-        //element is fluff, no content
-      }
-    } else if (courses[i][7].length == 1) {//if only one distribution, or distribution is separated by "or"
-      var distribs = courses[i][7];
-      distribs = distribs[0].split(" or ");
-      for (var j in distribs) {//in case there are 2 distributions split by "or"
-        if (distribs[j] == distribution) {
-          results.push(courses[i]);
-          break;//as long as one of the distributions matches the given, no need to go on
-        }
-      }
+  for (var i in courseArray) {
+    var courseFitsCriteria = true;
+    if (subject) {//if subject is a constraint, check subject against all courses
+      courseFitsCriteria = checkSubject(courseArray[i], searchSubjectQuery(subject));
+    }
+    if (courseFitsCriteria && distribution != "na") {//distributions is a constraint, check it against all courses
+      courseFitsCriteria = checkDistribs(courseArray[i], distribution);
+    }
+    if (courseFitsCriteria && days.length > 0) {//if days is a constraint, check it against all courses
+      courseFitsCriteria = checkDays(courseArray[i], days);
+    }
+    //at the very end, if courseFitsCriteria is still true, then the course has passed all
+    //tests for constraints. It may be appended onto resulting course array
+    if (courseFitsCriteria) {
+      results.push(courseArray[i]);
     }
   }
   return results;
 }
 
-function findCoursesALLConstraints(courses, subject, distribution, days) {
-
-  //call all three subjects, filtering the same set of courses so that at the end,
-  //the courses left fulfill all of the constraints.
-  if (subject) {
-    courses = findCoursesSub(courses, searchSubjectQuery(subject));
-  }
-  if (distribution != "na") {//placeholder for no distribution
-    courses = findCoursesDistrib(courses, distribution);
-  }
-  if (days.length > 0) {
-    courses = findCoursesDays(courses, days);
-  }
+function findCoursesALLConstraints(subject, distribution, days) {
+  //save all courses that fit the constraint into a variable
+  var courses = checkAllConstraints(subject, distribution, days);
 
   //empty any courses that filled the list div from past inquiries
   list.innerHTML = "";
 
   //fill (or refill) the list div of course divs showing name, title, 
   //instructor, days and times, and distributions.
-
-  if (courses.length == 0) {
+  if (courses.length == 0) {// if there are no courses, then 
     var obj = document.createElement("div");
     obj.className = "listElements";
 
@@ -218,39 +242,44 @@ function findCoursesALLConstraints(courses, subject, distribution, days) {
     obj.appendChild(alert);
     list.appendChild(obj);
   } else {
-    
     for (var i in courses) {
-      var obj = document.createElement("div");
+      var obj = document.createElement("button");
       obj.className = "listElements";
+      obj.id = courses[i][2]; // id name is the CRN
 
-      var courseName = document.createElement("h4");
-      courseName.innerHTML = courses[i][4];
-
-      var title = document.createElement("h5");
-      title.innerHTML = courses[i][0] + " " + courses[i][1] + " by " + courses[i][8];
+      var title = document.createElement("h1");
+      title.innerHTML = courses[i][0] + " " + courses[i][1];
+      var name = document.createElement("h2");
+      name.innerHTML = courses[i][4] + " with " + courses[i][8]; 
       
-      var distrib = document.createElement("h6");
-      var distribString = toStringDistribs(courses[i]);
-      console.log(distribString);
-      distrib.innerHTML = distribString;
+      var distribution = document.createElement("p");
+      distribution.innerHTML = toStringDistribs(courses[i]);
       
       var times = document.createElement("p");
       times.innerHTML = toStringDayAndTime(courses[i]);
 
-      obj.appendChild(courseName)
       obj.appendChild(title);
-      obj.appendChild(distrib);
+      obj.appendChild(name);
       obj.appendChild(times);
+      obj.appendChild(distribution);
       list.appendChild(obj);
     }
   }
 }
 
+function findCoursesByCRN(crn) {
+  for (var i in courseArray) {
+    if (courseArray[i][2] == crn) {
+      return courseArray[i];
+    }
+  }
+}
+
 //returns the abbreviation of the given course subjectt
-function searchSubjectQuery(query) {
+function searchSubjectQuery(sub) {
   var index;
   for (var i in subjects) {
-    if (query == subjects[i]) {
+    if (sub == subjects[i]) {
       index = i;
     }
   }
@@ -261,17 +290,13 @@ function searchSubjectQuery(query) {
 function toStringDayAndTime(course) {
   var string = "Meets on: ";
   for (var i in course[5]) {
-    string += course[5][i].replace(/R/, "Th") + " at " + course[6][i];
-    if (i + 1 != course[7].length) {
-      string += " and ";
-    }
-    console.log(string);
+    string += course[5][i].replace(/R/g, "Th") + " at " + course[6][i];
   }
   return string;
 }  
 
 function toStringDistribs(course) {
-  var string = "Fulfills ";
+  var string = "Fulfills: ";
   for (var i = 0; i < course[7].length; i++) {
     string += course[7][i];
     if (i + 1 != course[7].length) {
